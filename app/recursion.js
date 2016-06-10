5860d09480e4e0e623ae121c60b7e2c4ba828051
Ben Janes <brjanes@gmail.com>
2016-06-10 07:38:29 -0400

    Pass one recursion test

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,17 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    const files = [];
 
+    function addFiles(dirStructure) {
+      if (typeof dirStructure !== 'object') {
+        files.push(dirStructure);
+        return;
+      }
+      dirStructure.files.forEach(file => addFiles(file));
+    }
+    addFiles(data);
+    return files;
   },
 
   permute: function(arr) {
