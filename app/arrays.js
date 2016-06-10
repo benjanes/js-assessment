4a111ca2651a8640585e66e5a1682ada93f7c005
Ben Janes <brjanes@gmail.com>
2016-06-10 07:24:57 -0400

    Pass all arrays tests

--- app/arrays.js
+++ app/arrays.js
@@ -21,42 +21,54 @@ exports.arraysAnswers = {
   },
 
   append: function(arr, item) {
-
+    return arr.concat([item]);
   },
 
   truncate: function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend: function(arr, item) {
-
+    return [item].concat(arr);
   },
 
   curtail: function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat: function(arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert: function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count: function(arr, item) {
-
+    return arr.reduce((count, val) => {
+      if (val === item) count++;
+      return count;
+    }, 0);
   },
 
   duplicates: function(arr) {
-
+    return arr.reduce((dups, val) => {
+      if (arr.indexOf(val) !== arr.lastIndexOf(val) && !~dups.indexOf(val)) dups.push(val);
+      return dups;
+    }, []);
   },
 
   square: function(arr) {
-
+    return arr.map(val => val * val);
   },
 
   findAllOccurrences: function(arr, target) {
-
+    return arr.reduce((idxs, val, idx) => {
+      if (val === target) idxs.push(idx);
+      return idxs;
+    }, []);
   }
 };
