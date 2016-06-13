57b1d12950ed66e6382c65021691bab56c3a20ad
Ben Janes <brjanes@gmail.com>
2016-06-13 11:10:34 -0400

    Pass all recursion tests

--- app/recursion.js
+++ app/recursion.js
@@ -2,16 +2,25 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
-    const files = [];
-
-    function addFiles(dirStructure) {
-      if (typeof dirStructure !== 'object') {
-        files.push(dirStructure);
-        return;
+    var files = [];
+    var dirs = [];
+    
+    data.files.forEach(file => {
+      if (typeof file === 'string') {
+        files.push(file);
+      } else {
+        dirs.push(file);
       }
-      dirStructure.files.forEach(file => addFiles(file));
+    });
+    
+    for (var i = 0; i < dirs.length; i++) {
+      if (dirName && dirs[i].dir === dirName) {
+        return this.listFiles(dirs[i]);
+      } else {
+        files = files.concat(this.listFiles(dirs[i]));
+      }
     }
-    addFiles(data);
+    
     return files;
   },
 
@@ -35,10 +44,46 @@ exports.recursionAnswers = {
   },
 
   fibonacci: function(n) {
-
+    if (n < 1) return 0;
+    if (n === 1) return 1;
+    n--;
+    var res = 0;
+    var next = 0;
+    var memo = 0;
+    while(n) {
+      res = res ? res : 1;
+      next = res;
+      res += memo;
+      memo = next;
+      n--;
+    }
+    return res;
   },
 
   validParentheses: function(n) {
-
+    var parens = [];
+    var res = {};
+    while (n) {
+      parens = parens.concat(['(', ')']);
+      n--;
+    }
+    
+    (function findValid(chunk, remainder, open, close) {
+      if (chunk.length === parens.length) {
+        res[chunk] = true;
+        return;
+      }
+      if (close > open) return;
+      
+      remainder.forEach((char, idx) => {
+        var o = open;
+        var c = close;
+        if (char === '(') o += 1;
+        if (char === ')') c += 1;
+        findValid(chunk + char, remainder.slice(0, idx).concat(remainder.slice(idx + 1)), o, c);
+      });
+    })(parens[0], parens.slice(1), 1, 0);
+    
+    return Object.keys(res);
   }
 };
