c158a81811a77020979325474558712511bb5e87
Ben Janes <brjanes@gmail.com>
2016-06-13 09:43:45 -0400

    Pass regex tests

--- app/regex.js
+++ app/regex.js
@@ -2,26 +2,28 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    return /\d/g.test(str);
   },
 
   containsRepeatingLetter: function(str) {
-
+    return /([a-zA-z])\1+/g.test(str);
   },
 
   endsWithVowel: function(str) {
-
+    return /[aeiou]$/gi.test(str);
   },
 
   captureThreeNumbers: function(str) {
-
+    const series = str.match(/\d{3}/g);
+    if (series && series.length) return series[0];
+    return false;
   },
 
   matchesPattern: function(str) {
-
+    return /^\d{3}-\d{3}-\d{4}$/.test(str);
   },
 
   isUSD: function(str) {
-
+    return /^\$\d{1,3}(?:,?\d{3})*(?:\.\d{2})?$/.test(str);
   }
 };
